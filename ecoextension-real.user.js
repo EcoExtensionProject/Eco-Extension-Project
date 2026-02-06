0e5073b5548df977dacbf366f8d2a38bbd7218ad
marioledgar <marioledga@gmail.com>
2026-02-06 14:05:39 +0100

    initial commit

--- ecoextension-real.user.js
+++ ecoextension-real.user.js
@@ -1,2 +1,25 @@
-@name Eco extension
-@
+// ==UserScript==
+// @name         Eco Extension
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Eco extension with full everything
+// @match        *://*/*
+// @grant        none
+// @run-at       document-body
+// ==/UserScript==
+
+(function(){
+    'use strict';
+
+    //if (document.getElementById('bib-btn')) return;
+    // poner esto cuando esté el botón
+
+
+
+
+
+
+
+
+
+})();
